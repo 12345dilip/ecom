2cbe772875e96b2b2ff4b38d73ef93468ed9ae27
Hysas <hysas@Hysass-Mac-mini.bbrouter>
2021-10-28 13:39:17 +0530

    28

--- grocart/lib/Pages/three_dimention_view.dart
+++ grocart/lib/Pages/three_dimention_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
 
@@ -9,16 +11,26 @@ class ThreeDimentionView extends StatefulWidget {
 }
 
 class _ThreeDimentionViewState extends State<ThreeDimentionView> {
+  Object? camera;
+
+  @override
+  void initState() {
+    super.initState();
+    camera = Object(
+        fileName:
+            'images/Video_camera_v1_L3.123c1f10b2cc-f83f-47bc-a1de-3d85b88483ae/10128_Video_camera_v1_L3.obj');
+    camera!.rotation.setValues(-60, 150, 0);
+    camera!.updateTransform();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Cube(
           onSceneCreated: (Scene scene) {
-            scene.world.add(Object(
-                fileName:
-                    'images/Video_camera_v1_L3.123c1f10b2cc-f83f-47bc-a1de-3d85b88483ae/10128_Video_camera_v1_L3.obj'));
-            scene.camera.zoom = 20;
+            scene.world.add(camera!);
+            scene.camera.zoom = 7;
           },
         ),
       ),
